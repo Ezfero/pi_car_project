1e0c4c0cbbade1eea688b7e041f564e9776b1b76
Andrii <ezfero@gmail.com>
2025-08-31 13:23:46 +0100

    Better bootstrap

---
+++ CarApp/cpp_app/src/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+#include "greeter.grpc.pb.h" // Generated header from the .proto file
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
+
+class GreeterClient {
+public:
+    GreeterClient(std::shared_ptr<Channel> channel)
+        : stub_(Greeter::NewStub(channel)) {}
+
+    std::string SayHello(const std::string& user) {
+        HelloRequest request;
+        request.set_name(user);
+
+        HelloReply reply;
+        ClientContext context;
+
+        Status status = stub_->SayHello(&context, request, &reply);
+
+        if (status.ok()) {
+            return reply.message();
+        } else {
+            std::cout << "RPC failed: " << status.error_code() << ": " << status.error_message()
+                      << std::endl;
+            return "RPC failed";
+        }
+    }
+
+private:
+    std::unique_ptr<Greeter::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+    std::string target_address = "localhost:50051";
+    
+    // Create a client and connect to the gRPC server (the Python app)
+    GreeterClient client(grpc::CreateChannel(target_address, grpc::InsecureChannelCredentials()));
+    
+    std::string user = "World from C++";
+    std::string reply = client.SayHello(user);
+    std::cout << "Client received: " << reply << std::endl;
+    
+    return 0;
+}
--- MyPolyglotApp/cpp_app/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello from the C++ App!" << std::endl;
-    return 0;
-}
