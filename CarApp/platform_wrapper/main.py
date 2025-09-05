15ec10ed54574f536719fafbbfe245fe120a1642
Andrii Senkiv <oteron@MacBook-Air-2.local>
2025-09-05 16:48:03 +0100

    Renamed moduled

---
+++ CarApp/platform_wrapper/main.py
@@ -0,0 +1,30 @@
+import grpc
+import time
+from concurrent import futures
+
+# Import the generated gRPC files
+from apis.gen.py import greeter_pb2
+from apis.gen.py import greeter_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+class GreeterServicer(greeter_pb2_grpc.GreeterServicer):
+    def SayHello(self, request, context):
+        print(f"Server received: {request.name}")
+        return greeter_pb2.HelloReply(message=f"Hello, {request.name}!")
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    greeter_pb2_grpc.add_GreeterServicer_to_server(GreeterServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    print("gRPC server started on port 50051. Press Ctrl+C to stop.")
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == '__main__':
+    print("Hello from the Python App!")
+    serve()
