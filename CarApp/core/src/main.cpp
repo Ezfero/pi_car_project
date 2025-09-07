dbdfe815c2f3064bfbe942c3c50bec4889d980ab
Andrii Senkiv <ezfero@gmail.com>
2025-09-07 18:24:43 +0100

    Integrating command dispatcher

--- CarApp/core/src/main.cpp
+++ CarApp/core/src/main.cpp
@@ -10,48 +10,24 @@ import orchestration;
 #include "movement.grpc.pb.h"
 #include "movement/StdInputHandler.h"
 #include "dependency_injection/ServiceProvider.h"
+#include "movement/CommandDispatcher.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
-using car_ipc::Greeter;
-using car_ipc::HelloReply;
-using car_ipc::HelloRequest;
 using namespace car_app::core;
-
-class GreeterClient {
-public:
-    GreeterClient(std::shared_ptr<Channel> channel)
-        : stub_(Greeter::NewStub(channel)) {}
-
-    std::string SayHello(const std::string& user) {
-        HelloRequest request;
-        request.set_name(user);
-
-        HelloReply reply;
-        ClientContext context;
-
-        Status status = stub_->SayHello(&context, request, &reply);
-
-        if (status.ok()) {
-            return reply.message();
-        } else {
-            std::cout << "RPC failed: " << status.error_code() << ": " << status.error_message()
-                      << std::endl;
-            return "RPC failed";
-        }
-    }
-
-private:
-    std::unique_ptr<Greeter::Stub> stub_;
-};
+using namespace car_ipc;
 
 int main(int argc, char** argv) {
     std::cout << "Starting CarApp " << std::endl;
 
+    std::string target_address = "localhost:50051";
+    auto client = MovementClient::NewStub(grpc::CreateChannel(target_address, grpc::InsecureChannelCredentials()));
+
     ServiceProvider::instance()
         .registerAs<IInputHandler>(std::make_shared<StdInputHandler>())
-        .registerAs<ControlOrchestrator>(std::make_shared<ControlOrchestrator>());
+        .registerAs<ControlOrchestrator>(std::make_shared<ControlOrchestrator>())
+        .registerAs<ICommandDispatcher>(std::make_shared<CommandDispatcher>(std::move(client)));
     
     std::thread orchestrator_thread([]() {
         ServiceProvider::instance().get<ControlOrchestrator>()->start();
@@ -63,15 +39,6 @@ int main(int argc, char** argv) {
 
     ServiceProvider::instance().get<IInputHandler>()->start(handlerCallback);
     orchestrator_thread.join();
-
-    // std::string target_address = "localhost:50051";
-    
-    // // Create a client and connect to the gRPC server (the Python app)
-    // GreeterClient client(grpc::CreateChannel(target_address, grpc::InsecureChannelCredentials()));
-    
-    // std::string user = "World from C++";
-    // std::string reply = client.SayHello(user);
-    // std::cout << "Client received: " << reply << std::endl;
     
     std::cout << "Finishing CarApp " << std::endl;
     return 0;
